3b48b6a539b1da51444137676d19b0e0aa01990a
TravisNeils <tra5is98@gmail.com>
2021-02-01 07:00:30 -0800

    finished asssignment I believe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TextInput from './TextInput';
+function addmessage(messages1, text){
+  messages1.push({text:text});
+  return messages1;
+}
 
 function App() {
+  let [messages, setMessages] = useState([{ text: 'hello' }])
+  console.log(messages)
+  let message_text = messages.map((message) => message.text)
+  console.log(message_text)
+  let listItems = messages.map((message) => 
+      <li>{message.text}</li>
+      );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='icon'></div>
+        <div>PLATTER</div>
       </header>
+      <div className="messagebox">
+        <p className='subtext'>Try adding more messages it works!</p>
+        <ul className="messages">
+          <li>
+            Hello my friend
+          </li>
+          <li>
+            Hello to you too
+          </li>
+          {messages.map((message) => 
+      <li>{message.text}</li>)}
+        </ul>
+        {/* {messages[0].text} */}
+      </div>
+      <TextInput
+        send={(t) => setMessages([...messages, {text: t}])}
+      />
+      <div>
+
+      </div>
     </div>
   );
 }
